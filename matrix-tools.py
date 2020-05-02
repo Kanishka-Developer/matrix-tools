2c9cad10e6def34c9d38ba35d246dad4eac06279
Kanishka_Developer <kanishka.developer@gmail.com>
2020-05-02 17:26:37 +0530

    Seems to work, IDK

--- matrix-tools.py
+++ matrix-tools.py
@@ -1,7 +1,16 @@
 # Matrix Tools
 # by Kanishka_Developer
 
-def btfy(matrix, maxLength): # beautifies matrix for easier viewing
+def maxLength(matrix): # returns length of longest item in matrix
+	maxLength = 0
+	for i in range (0, len(matrix)):
+		for j in range (0, len(matrix[i])):
+			if len(str(matrix[i][j])) > maxLength:
+				maxLength = len(str(matrix[i][j]))
+	return maxLength
+
+def btfy(matrix): # beautifies matrix for easier viewing
+	mL = maxLength(matrix)
 	op = ""
 	for l in list(matrix):
 		if l != matrix[len(matrix)-1]:
@@ -9,7 +18,7 @@ def btfy(matrix, maxLength): # beautifies matrix for easier viewing
 			for i in list(l):
 				if i != matrix[len(matrix)-1]:
 					s = " "
-					for v in range(0, maxLength + 1 - len(str(i))):
+					for v in range(0, mL + 1 - len(str(i))):
 						s = s + " "
 					x = x + str(i) + s
 				else:
@@ -20,7 +29,7 @@ def btfy(matrix, maxLength): # beautifies matrix for easier viewing
 			for i in list(l):
 				if i != matrix[len(matrix)-1]:
 					s = " "
-					for v in range(0, maxLength + 1 - len(str(i))):
+					for v in range(0, mL + 1 - len(str(i))):
 						s = s + " "
 					x = x + str(i) + s
 				else:
@@ -28,11 +37,11 @@ def btfy(matrix, maxLength): # beautifies matrix for easier viewing
 			op = op + x
 	return "\n" + op + "\n"
 
-def rt90CW(matrix, maxLength):
+def rt90CW(matrix): # rotates matrix by 90 degrees clockwise
 	rC = len(matrix)
 	cC = len(matrix[0])
-	print("Matrix:\n{}".format(btfy(matrix, maxLength)))
-	print("Matrix size: {} x {}".format(cC, rC))
+	#print("Matrix:\n{}".format(btfy(matrix)))
+	#print("Matrix size: {} x {}".format(cC, rC))
 	rC, cC = cC, rC
 	nMatrix = []
 	for i in range(0, rC):
@@ -42,11 +51,11 @@ def rt90CW(matrix, maxLength):
 		nMatrix.append(row)
 	return nMatrix
 
-def rt90AC(matrix, maxLength):
+def rt90AC(matrix): # rotates matrix by 90 degrees anti-clockwise
 	rC = len(matrix)
 	cC = len(matrix[0])
-	print("Matrix:\n{}".format(btfy(matrix, maxLength)))
-	print("Matrix size: {} x {}".format(cC, rC))
+	#print("Matrix:\n{}".format(btfy(matrix)))
+	#print("Matrix size: {} x {}".format(cC, rC))
 	rC, cC = cC, rC
 	nMatrix = []
 	for i in range(rC-1, -1, -1):
@@ -56,11 +65,11 @@ def rt90AC(matrix, maxLength):
 		nMatrix.append(row)
 	return nMatrix
 
-def rtCW(matrix, maxLength):
+def rtCW(matrix): # rotates matrix by one item clockwise; works only for NxN matrices
 	rC = len(matrix)
 	cC = len(matrix[0])
-	print("Matrix:\n{}".format(btfy(matrix, maxLength)))
-	print("Matrix size: {} x {}".format(cC, rC)) # you're screwed if input isn't an NxN matrix. trust me on that one.
+	#print("Matrix:\n{}".format(btfy(matrix)))
+	#print("Matrix size: {} x {}".format(cC, rC))
 	lC = int(max(rC,  cC) / 2) if max(rC,  cC) % 2 == 0 else int((max(rC, cC) - 1) / 2)
 	for l in range(1, lC+1):
 		for i in range(l-1, rC-l):
@@ -69,11 +78,11 @@ def rtCW(matrix, maxLength):
 			matrix[i].insert(cC-l, (matrix[i-1].pop(cC-l))) if (i != l) else matrix[i].insert(cC-l, (matrix[i-1].pop(cC-l+1)))
 	return matrix
 
-def rtAC(matrix, maxLength):
+def rtAC(matrix): # rotates matrix by one item anti-clockwise; works only for NxN matrices
 	rC = len(matrix)
 	cC = len(matrix[0])
-	print("Matrix:\n{}".format(btfy(matrix, maxLength)))
-	print("Matrix size: {} x {}".format(cC, rC)) # you're screwed if input isn't an NxN matrix. trust me on that one.
+	#print("Matrix:\n{}".format(btfy(matrix)))
+	#print("Matrix size: {} x {}".format(cC, rC))
 	lC = int(max(rC,  cC) / 2) if max(rC,  cC) % 2 == 0 else int((max(rC, cC) - 1) / 2)
 	for l in range(1, lC+1):
 		for i in range(rC-l, l-1, -1):
@@ -82,11 +91,11 @@ def rtAC(matrix, maxLength):
 			matrix[i].insert(cC-l, (matrix[i+1].pop(cC-l))) if (i != rC-l-1) else matrix[i].insert(cC-l, (matrix[i+1].pop(cC-l+1)))
 	return matrix
 
-def fpVT(matrix, maxLength):
+def fpVT(matrix): # flips matrix vertically
 	rC = len(matrix)
 	cC = len(matrix[0])
-	print("Matrix:\n{}".format(btfy(matrix, maxLength)))
-	print("Matrix size: {} x {}".format(cC, rC))
+	#print("Matrix:\n{}".format(btfy(matrix)))
+	#print("Matrix size: {} x {}".format(cC, rC))
 	if rC % 2 == 0:
 		l = int(rC/2)
 	else:
@@ -96,11 +105,11 @@ def fpVT(matrix, maxLength):
 		matrix[i], matrix[j] = matrix[j], matrix[i]
 	return matrix
 
-def fpHZ(matrix, maxLength):
+def fpHZ(matrix): # flips matrix horizontally
 	rC = len(matrix)
 	cC = len(matrix[0])
-	print("Matrix:\n{}".format(btfy(matrix, maxLength)))
-	print("Matrix size: {} x {}".format(cC, rC))
+	#print("Matrix:\n{}".format(btfy(matrix)))
+	#print("Matrix size: {} x {}".format(cC, rC))
 	if cC % 2 == 0:
 		l = int(cC/2)
 	else:
@@ -113,12 +122,13 @@ def fpHZ(matrix, maxLength):
 
 matrix3x3 = [[1,2,3],[4,5,6],[7,8,9]]
 matrix4x4 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-matrix4x3 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]] #! matrix-tools is designed for NxN matrices !
+matrix4x3 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]] #! matrix-tools is designed primarily for NxN matrices !
 
-#print(btfy(rt90CW(matrix4x4, 4), 4))
-#print(btfy(rt90AC(matrix4x4, 4), 4))
-#print(btfy(matrix4x4, 4))
-#print(btfy(rtCW(matrix4x4, 4), 4))
-#print(btfy(rtAC(matrix4x4, 4), 4))
-#print(btfy(fpVT(matrix4x4, 4), 4))
-#print(btfy(fpHZ(matrix4x4, 4), 4))
+#print(maxLength(matrix4x4))
+#print(btfy(matrix4x4))
+#print(btfy(rt90CW(matrix4x4)))
+#print(btfy(rt90AC(matrix4x4)))
+#print(btfy(rtCW(matrix4x4)))
+#print(btfy(rtAC(matrix4x4)))
+#print(btfy(fpVT(matrix4x4)))
+#print(btfy(fpHZ(matrix4x4)))
